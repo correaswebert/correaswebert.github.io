b3db683620139f4c4467e92a0cdfb5d81dc5deb1
Swebert Correa <correaswebert@gmail.com>
2021-01-13 23:42:19 +0530

    feat: revamped whole website

---
+++ assets/fonts/hokaide/README.txt
@@ -0,0 +1,6 @@
+This font is for PERSONAL USE ONLY , not allowed for commercial use in any form.
+
+If you need a CUSTOM LICENSE for commercial or CORPORATE LICENSE please contact us at : jadaakbaaal@gmail.com
+
+Thank you!
+Jadatype
---
+++ assets/scripts/main.js
@@ -0,0 +1,83 @@
+const loader = document.querySelector(".loader");
+const mainContent = document.getElementById("main-content");
+const linkContent = document.getElementById("link-content");
+let revealed = false;
+
+const blogLinks = [
+  { title: "Medium", url: "https://correaswebert.medium.com/" },
+  { title: "DevTo", url: "https://dev.to/correaswebert" },
+  { title: "Hashnode", url: "https://hashnode.com/@correaswebert" },
+  { title: "Personal", url: "https://blog.swebert.codes" },
+];
+
+const socialLinks = [
+  { title: "Twitter", url: "https://twitter.com/@correaswebert" },
+  { title: "Instagram", url: "https://www.instagram.com/correaswebert/" },
+  { title: "LinkedIn", url: "https://in.linkedin.com/in/correaswebert" },
+];
+
+const codeLinks = [
+  { title: "Github", url: "https://github.com/correaswebert/" },
+  { title: "Gitlab", url: "https://gitlab.com/correaswebert/" },
+];
+
+const creditLinks = [
+  {
+    title: "Music",
+    url: "https://www.youtube.com/channel/UCOR-iqlcedb8VMnax37m3MQ",
+  },
+  { title: "Fonts", url: "https://www.1001fonts.com/" },
+  { title: "Icons", url: "https://www.flaticon.com/" },
+  { title: "Background", url: "https://www.svgbackgrounds.com/" },
+  { title: "Animations", url: "https://codepen.io/firestar300/pen/zLKZVZ" },
+];
+
+function populateLinks(content) {
+  linkContent.innerHTML = `<button onclick="reveal()" class="close-button">&Cross;</button>`;
+
+  switch (content) {
+    case "blog":
+      blogLinks.forEach((elem) => {
+        linkContent.innerHTML += `<a class="link" href="${elem.url}">${elem.title}</a>`;
+      });
+      break;
+
+    case "social":
+      socialLinks.forEach((elem) => {
+        linkContent.innerHTML += `<a class="link" href="${elem.url}">${elem.title}</a>`;
+      });
+      break;
+
+    case "code":
+      codeLinks.forEach((elem) => {
+        linkContent.innerHTML += `<a class="link" href="${elem.url}">${elem.title}</a>`;
+      });
+      break;
+
+    case "credit":
+      creditLinks.forEach((elem) => {
+        linkContent.innerHTML += `<a class="link" href="${elem.url}">${elem.title}</a>`;
+      });
+      break;
+
+    default:
+      break;
+  }
+}
+
+function reveal(content) {
+  loader.classList.add("loader--active");
+  populateLinks(content);
+
+  setTimeout(function () {
+    if (revealed) {
+      mainContent.style.display = "flex";
+      linkContent.style.display = "none";
+    } else {
+      mainContent.style.display = "none";
+      linkContent.style.display = "flex";
+    }
+    loader.classList.remove("loader--active");
+    revealed = !revealed;
+  }, 2000);
+}
